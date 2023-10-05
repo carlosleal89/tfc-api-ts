67985ec50aed757223feb926872433d3c54ac7f1
Carlos Leal <carlos.frleal@gmail.com>
2023-10-05 16:20:19 -0300

    req 22 em andamento

--- app/backend/src/mocks/Matches.mock.ts
+++ app/backend/src/mocks/Matches.mock.ts
@@ -13,6 +13,40 @@ const match = {
   },
 };
 
+const matchInProgress = {
+  id: 41,
+  homeTeamId: 16,
+  homeTeamGoals: 2,
+  awayTeamId: 9,
+  awayTeamGoals: 0,
+  inProgress: true,
+  homeTeam: {
+    teamName: 'São Paulo',
+  },
+  awayTeam: {
+    teamName: 'Internacional',
+  },
+};
+
+const matchFinished = {
+  id: 1,
+  homeTeamId: 16,
+  homeTeamGoals: 1,
+  awayTeamId: 8,
+  awayTeamGoals: 1,
+  inProgress: false,
+  homeTeam: {
+    teamName: 'São Paulo',
+  },
+  awayTeam: {
+    teamName: 'Grêmio',
+  },
+};
+
+const matchesFinished = [matchFinished];
+
+const matchesInProgress = [matchInProgress];
+
 const matches = [match];
 
-export { match, matches };
+export { match, matches, matchInProgress, matchesInProgress, matchFinished, matchesFinished };
--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -8,11 +8,14 @@ import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
 
-import { token } from '../mocks/Users.mock';
+import * as jwt from 'jsonwebtoken';
+
+import envArgs from '../utils/envArgs';
+import { jwtPayload } from '../mocks/Users.mock';
 
 import SequelizeMatch from '../database/models/SequelizeMatch';
 
-import { match, matches } from '../mocks/Matches.mock';
+import { match, matches, matchInProgress, matchesInProgress, matchesFinished } from '../mocks/Matches.mock';
 
 chai.use(chaiHttp);
 
@@ -27,5 +30,43 @@ describe('Testes de rota Match', () => {
     
     expect(status).to.be.equal(200);
     expect(body).to.deep.equal(matches);
-  })
+  });
+
+  it('Testa se o endpoint get /matches?inProgress=true retorna todas as matches em progresso.', async function () {
+    sinon.stub(SequelizeMatch, 'findAll').resolves(matchesInProgress as any);
+    
+    const { body, status } = await chai.request(app).get('/matches?inProgress=true');       
+    
+    expect(status).to.be.equal(200);
+    expect(body).to.deep.equal(matchesInProgress);
+  });
+
+  it('Testa se o endpoint get /matches?inProgress=false retorna todas as matches em progresso.', async function () {
+    sinon.stub(SequelizeMatch, 'findAll').resolves(matchesFinished as any);
+    
+    const { body, status } = await chai.request(app).get('/matches?inProgress=false');       
+    
+    expect(status).to.be.equal(200);
+    expect(body).to.deep.equal(matchesFinished);
+  });
+
+  it('Testa se o endpoint patch /matches/id/finish finaliza uma match com sucesso.', async function () {   
+    const validToken = jwt.sign(jwtPayload, envArgs.jwtSecret);    
+    
+    const { status, body } = await chai.request(app).patch('/matches/43/finish').set('Authorization', `Bearer ${validToken}`);    
+        
+    expect(status).to.be.equal(200);
+    expect(body).to.deep.equal({
+      message: "Finished"
+    });
+  });
+
+  it('Testa se o endpoint patch /matches/id/finish retorna a mensagem esperada ao não informar um token.', async function () {
+    const { status, body } = await chai.request(app).patch('/matches/43/finish');    
+        
+    expect(status).to.be.equal(401);
+    expect(body).to.deep.equal({
+      message: "Token not found"
+    });
+  });
 });
